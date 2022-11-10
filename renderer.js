f19b3458b3a45f0b325e9fee1aa0946534e2d0b6
marcos <mc@mc.com>
2022-11-10 16:35:21 -0300

    first commit

---
+++ renderer.js
@@ -0,0 +1,60 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+const { SerialPort } = require('serialport')
+const tableify = require('tableify')
+
+// Port variable to store the SerialPort interface
+let port;
+
+// Callback to read the port path where to write
+// The user must select the correct one
+function updatePort() {
+  portId = document.getElementById("port-id");
+  console.log(portId.value);
+  port = new SerialPort({ path: portId.value, baudRate: 9600 });
+}
+
+// Function to send data to the port
+function sendData(p) {
+  console.log("Enviando datos...");
+    
+  port.write('Toggle port '+ p, function(err) {
+    if (err) {
+      return console.log('Error on write: ', err.message)
+    }
+    console.log('message written')
+  })
+}
+
+// Function to get all the ports available in the PC
+async function listSerialPorts() {
+  await SerialPort.list().then((ports, err) => {
+    if(err) {
+      document.getElementById('error').textContent = err.message
+      return
+    } else {
+      document.getElementById('error').textContent = ''
+    }
+    console.log('ports', ports);
+
+    if (ports.length === 0) {
+      document.getElementById('error').textContent = 'No ports discovered'
+    }
+
+    tableHTML = tableify(ports)
+    document.getElementById('ports').innerHTML = tableHTML
+  })
+}
+
+function listPorts() {
+  //listSerialPorts();
+  //setTimeout(listPorts, 2000);
+}
+
+// Set a timeout that will check for new serialPorts every 2 seconds.
+// This timeout reschedules itself.
+//setTimeout(listPorts, 2000);
+
+listSerialPorts()
